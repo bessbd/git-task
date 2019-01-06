459168d426efb259d010c5bd6520d444d4aa4bde
Bessenyei Balázs Donát <bessbd@gmail.com>
2019-01-06 02:36:41 +0100

    Add basic query capabilities

--- gittask/GitTask.py
+++ gittask/GitTask.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+
 import datetime
 import logging
 import os
 import random
 import shlex
+import shutil
 import string
 import subprocess
+import tempfile
 
 import fire
 import parsedatetime
+import whoosh
+import whoosh.fields
+import whoosh.index
+import whoosh.qparser
 import yaml
 
 
@@ -35,8 +43,9 @@ class GitTask:
                 deadline)
             deadline = datetime.datetime(*time_struct[:6]).isoformat()
         self.__task_list = self.__list_default_tasks() + [
-            self.__normalize_task(summary, assignee=assignee,
-                                  deadline=deadline)]
+            TaskItem.dictify_and_extend_task(summary,
+                                             assignee=assignee,
+                                             deadline=deadline)]
         self.__save()
 
     def ls(self):
@@ -53,6 +62,7 @@ class GitTask:
 
     def rm(self, id):
         """Remove one task"""
+
         def __id_matches(item):
             [(key, details)] = item.items()
             return 'id' in details and details['id'].startswith(id)
@@ -73,6 +83,37 @@ class GitTask:
             self.__task_list.remove(items[0])
             self.__save()
 
+    def q(self, querystr):
+        """
+        Find tasks
+
+        For complex queries, `Whoosh query language`_ can be used.
+
+
+        .. _Whoosh query language:
+            https://whoosh.readthedocs.io/en/latest/querylang.html
+        """
+        schema = whoosh.fields.Schema(title=whoosh.fields.TEXT(stored=True),
+                                      path=whoosh.fields.ID(stored=True),
+                                      content=whoosh.fields.TEXT(stored=True))
+        temp_dir = tempfile.mkdtemp()
+        whoosh_index = whoosh.index.create_in(temp_dir, schema)
+        whoosh_index_writer = whoosh_index.writer()
+        task_object_list = [TaskItem(item) for item in
+                            self.__list_default_tasks()]
+        for i, task_object in enumerate(task_object_list):
+            whoosh_index_writer.add_document(title=task_object.get_key(),
+                                             path=str(i),
+                                             content=task_object.
+                                             get_yaml_repr())
+        whoosh_index_writer.commit()
+        with whoosh_index.searcher() as searcher:
+            query = whoosh.qparser.QueryParser("content", whoosh_index.schema
+                                               ).parse(querystr)
+            for result in searcher.search(query):
+                print(result.get("content"))
+        shutil.rmtree(temp_dir)
+
     @staticmethod
     def install_git_alias():
         """
@@ -90,56 +131,59 @@ class GitTask:
         subprocess.check_call(
             shlex.split("git config --global --unset-all alias.task"))
 
-    @staticmethod
-    def config_get(key):
-        """Get a config variable"""
-        pass
-
-    @staticmethod
-    def config_set(key, value):
-        """Set a config variable"""
-        pass
-
-    @staticmethod
-    def config_global_get(key):
-        """Get a config variable"""
-        pass
-
-    @staticmethod
-    def config_global_set(key, value):
-        """Set a config variable"""
-        pass
-
     def __list_default_tasks(self):
         return self.__task_list or []
-
-    @staticmethod
-    def __normalize_task(item, **kwargs):
-        def __gen_id():
-            return ''.join(
-                random.SystemRandom().choices(string.ascii_lowercase, k=8))
-
-        if type(item) is str:
-            item = {item: {}}
-        if type(item) is not dict:
-            raise ValueError("Unexpected item type")
-
-        [(key, details)] = item.items()
-        details.setdefault("id", __gen_id())
-        details.update({k: v for k, v in kwargs.items() if v is not None})
-        return {key: {k: v for k, v in details.items() if v is not None}}
 
     def __save(self):
         if self.__task_list is not None:
             with(open(self.__TASKS_FILE_NAME, 'w')) as tasks_file:
                 self.__serialize_list(self.__task_list, stream=tasks_file)
 
-    def __serialize_list(self, item_list, **kwargs):
+    @staticmethod
+    def __serialize_list(item_list, **kwargs):
         return yaml.dump(
-            [self.__normalize_task(item) for item in item_list],
+            [TaskItem.dictify_and_extend_task(item) for item in
+             item_list],
             default_flow_style=False,
             **kwargs
         )
+
+
+class TaskItem:
+    """Class representing one GitTask item"""
+
+    def __init__(self, item, **kwargs):
+        self.__item = TaskItem.dictify_and_extend_task(item, **kwargs)
+        [(self.__key, self.__details)] = self.__item.items()
+
+    def get_item(self):
+        return self.__item
+
+    def get_yaml_repr(self):
+        return yaml.dump(self.__item, default_flow_style=False)
+
+    def get_key(self):
+        return self.__key
+
+    def get_details(self):
+        return self.__details
+
+    @staticmethod
+    def __gen_id():
+        return ''.join(
+            random.SystemRandom().choices(string.ascii_lowercase, k=8))
+
+    @staticmethod
+    def dictify_and_extend_task(item, **kwargs):
+        if type(item) is str:
+            item = {item: {}}
+        if type(item) is not dict:
+            raise ValueError("Unexpected item type")
+
+        [(key, details)] = item.items()
+        details.setdefault("id", TaskItem.__gen_id())
+        details.update({k: v for k, v in kwargs.items() if v is not None})
+        return {key: {k: v for k, v in details.items() if v is not None}}
 
 
 def main():  # Required for entry_points in setup.py
